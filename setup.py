a44c62a15b4be856a4094c5987332c168961154f
Yue1Harriet1Huang <dariahuangster@gmail.com>
2020-05-02 03:05:09 +0000

    make repo a pip package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='app_credential_management',
+    version='0.0.1',
+    description="help to set up your app's credentials through environment variables",
+    url='https://github.com/Yue1Harriet1Huang/app_credential_management.git',
+    author='Yue Harriet Huang',
+    author_email='dariahuangster@gmail.com',
+    license='unlicense',
+    packages=['app_credential_management'],
+    zip_safe=False
+)
